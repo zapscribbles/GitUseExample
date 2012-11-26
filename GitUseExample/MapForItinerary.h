2576e2f027f57ec8fd2e32421d58adf001a225e6
Stephen Zappia <stephenzappia@minimac.fritz.box>
2012-11-26 21:33:06 +1100

    Lets branch out...started working on a new map feature but not quite done yet

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by Stephen Zappia on 26/11/12.
+//  Copyright (c) 2012 Stephen Zappia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUseExample
+//
+//  Created by Stephen Zappia on 26/11/12.
+//  Copyright (c) 2012 Stephen Zappia. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
